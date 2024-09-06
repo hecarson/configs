73005d70d62e6b57b2b41da998528b5065c7f71e
hecarson <hecarson3@gmail.com>
2024-09-06 13:59:15 -0600

    nvim keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,13 +1,13 @@
--- explore
+-- Explore
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
--- copy/paste with system clipboard
+-- Copy/paste with system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>yy", [["+yy]])
 vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
 vim.keymap.set({"n", "v"}, "<leader>P", [["+P]])
 
--- no highlight search
+-- No highlight search
 vim.keymap.set("n", "<leader>n", function()
     vim.cmd("nohlsearch")
 end)
@@ -21,3 +21,6 @@ vim.keymap.set("n", "<leader>lc", function()
     vim.cmd("w")
     vim.cmd("!pdflatex %")
 end)
+
+-- Faster exit from terminal mode
+vim.keymap.set("t", "<A-[>", "<C-\\><C-N>")

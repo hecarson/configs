b8654b434ae063e50f039d142735f1627c7276d3
hecarson <hecarson3@gmail.com>
2024-10-06 14:59:21 -0600

    nvim move keymaps to keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,4 +1,4 @@
--- explore
+-- Explore
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 -- Copy/paste with system clipboard
@@ -23,3 +23,23 @@ end)
 
 -- Faster exit from terminal mode
 vim.keymap.set("t", "<A-[>", "<C-\\><C-N>")
+
+-- Plugin keymaps
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- find file
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- find grep
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {}) -- find help
+
+local dap = require("dap")
+vim.keymap.set("n", "<F5>", function() dap.continue() end)
+vim.keymap.set("n", "<F8>", function() dap.step_over() end)
+vim.keymap.set("n", "<F7>", function() dap.step_into() end)
+vim.keymap.set("n", "<S-F7>", function() dap.step_out() end)
+vim.keymap.set("n", "<leader>dr", function() dap.repl.open() end) -- debug REPL
+vim.keymap.set("n", "<leader>dq", function() dap.terminate() end) -- debug quit
+vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end) -- debug breakpoint
+
+local dapui = require("dapui")
+vim.keymap.set("n", "<leader>dt", dapui.toggle) -- debug toggle
--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -2,16 +2,6 @@ return {
     {
         "mfussenegger/nvim-dap",
         config = function()
-            -- Keymaps
-            local dap = require("dap")
-            vim.keymap.set("n", "<F5>", function() dap.continue() end)
-            vim.keymap.set("n", "<F8>", function() dap.step_over() end)
-            vim.keymap.set("n", "<F7>", function() dap.step_into() end)
-            vim.keymap.set("n", "<S-F7>", function() dap.step_out() end)
-            vim.keymap.set("n", "<leader>dr", function() dap.repl.open() end) -- debug REPL
-            vim.keymap.set("n", "<leader>dq", function() dap.terminate() end) -- debug quit
-            vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end) -- debug breakpoint
-
             -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#ccrust-via-gdb
 
             -- Adapters
@@ -19,7 +9,7 @@ return {
             dap.adapters.gdb = {
                 type = "executable",
                 command = "gdb",
-                args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+                args = { "-n", "--interpreter=dap", "--eval-command", "set print pretty on" },
             }
 
             -- Configuration
@@ -29,7 +19,7 @@ return {
                     type = "gdb",
                     request = "launch",
                     program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
                     end,
                     cwd = "${workspaceFolder}",
                     stopAtBeginningOfMainSubprogram = false,
@@ -45,9 +35,6 @@ return {
         config = function()
             local dapui = require("dapui")
             dapui.setup()
-            
-            -- debug toggle
-            vim.keymap.set("n", "<leader>dt", dapui.toggle)
 
             -- For some reason, this fixes the weird black background of the DAP REPL icons
             apply_colorscheme()

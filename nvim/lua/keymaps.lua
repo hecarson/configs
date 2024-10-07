f5fb2afe9d4a59414d23470c23486246419b7e60
hecarson <hecarson3@gmail.com>
2024-10-07 15:57:43 -0600

    change dap keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -34,9 +34,9 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {}) -- find help
 
 local dap = require("dap")
 vim.keymap.set("n", "<F5>", function() dap.continue() end)
-vim.keymap.set("n", "<F8>", function() dap.step_over() end)
-vim.keymap.set("n", "<F7>", function() dap.step_into() end)
-vim.keymap.set("n", "<S-F7>", function() dap.step_out() end)
+vim.keymap.set("n", "<F10>", function() dap.step_over() end)
+vim.keymap.set("n", "<F11>", function() dap.step_into() end)
+vim.keymap.set("n", "<S-F11>", function() dap.step_out() end)
 vim.keymap.set("n", "<leader>dr", function() dap.repl.open() end) -- debug REPL
 vim.keymap.set("n", "<leader>dq", function() dap.terminate() end) -- debug quit
 vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end) -- debug breakpoint

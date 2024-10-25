1e0ff2e3f859660074e72f84c8a8dec9bd4f7065
hecarson <hecarson3@gmail.com>
2024-10-25 02:07:55 -0600

    find symbols keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -31,6 +31,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) -- find file
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- find grep
 -- vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {}) -- find help
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {}) -- find symbols
 vim.keymap.set("n", "<leader>ft", builtin.treesitter, {}) -- find treesitter
 
 local dap = require("dap")

e0ec4aa8814638d54267c782198d1bfee8b1dc48
hecarson <hecarson3@gmail.com>
2024-11-04 19:40:22 -0700

    disable left click

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
 vim.keymap.set({"n", "v"}, "<leader>P", [["+P]])
 
+-- Disable ctrl + left mouse
+vim.keymap.set("n", "<C-LeftMouse>", function() end)
+
 -- no highlight search
 vim.keymap.set("n", "<leader>n", function()
     vim.cmd("nohlsearch")
@@ -38,7 +41,7 @@ local dap = require("dap")
 vim.keymap.set("n", "<F5>", function() dap.continue() end)
 vim.keymap.set("n", "<F10>", function() dap.step_over() end)
 vim.keymap.set("n", "<F11>", function() dap.step_into() end)
-vim.keymap.set("n", "<S-F11>", function() dap.step_out() end)
+vim.keymap.set("n", "<F23>", function() dap.step_out() end) -- shift + f11, inserted with CTRL-K
 vim.keymap.set("n", "<leader>dr", function() dap.repl.open() end) -- debug REPL
 vim.keymap.set("n", "<leader>dq", function() dap.terminate() end) -- debug quit
 vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end) -- debug breakpoint

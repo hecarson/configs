d5e285d0fbb025a8eae19ab1d2e137d8c18b3d5e
hecarson <hecarson3@gmail.com>
2025-02-16 02:18:56 -0700

    disable treesitter auto install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -17,7 +17,7 @@ return {
         require("nvim-treesitter.configs").setup({
             ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
             sync_install = false,
-            auto_install = true,
+            auto_install = false,
             highlight = {
                 enable = true,
 
